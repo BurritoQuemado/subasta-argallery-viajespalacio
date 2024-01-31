54017ee11afa8e67b5b037fc687ea5a2675b2606
Diego Flores <dfmonroy07@gmai.com>
2024-01-31 11:06:08 -0600

    updated to viajes palacio

--- subastas-stands-app/src/components/maincontent/maincontent.jsx
+++ subastas-stands-app/src/components/maincontent/maincontent.jsx
@@ -56,16 +56,13 @@ function MainContent({logged, user_id}) {
                             <div className="px-6 py-4 sm:px-6 sm:py-32 lg:px-8">
                                 <div className="mx-auto max-w-2xl text-center">
                                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                                        Esta es tu guía de arte interactivo LICON.
+                                        Esta es tu guía de arte interactivo Viajes Palacio.
                                     </h2>
                                     <p className="mx-auto mt-6 max-w-xl text-lg leading-6 text-gray-600 text-justify">
                                         <b>Galería</b> es un recorrido por las distintas obras que puedes encontrar en nuestra galería.
                                     </p>
                                     <p className="mx-auto mt-6 max-w-xl text-lg leading-6 text-gray-600 text-justify">
-                                        <b>Muro del reconocimiento</b> es un recorrido por las distintas explicaciones de los elementos que llegaron a inspirar estas obras y un reconocimiento a los protagonistas de los avances en la inmunohematología universal y mexicana.
-                                    </p>
-                                    <p className="mx-auto mt-6 max-w-xl text-lg leading-6 text-gray-600 text-justify">
-                                        <b>Cartera Cryptolicoins</b> Es una cartera de monedas digitales que podrá adquirir visitando nuestro stand y usarlas para subastar alguna de nuestras obras.
+                                        <b>Cartera</b> Es una cartera de monedas digitales que podrá adquirir contestando las preguntas que se presentaran después de la premiación y usarlas para subastar por alguna de nuestras obras.
                                     </p>
                                     <div className="mt-10 grid grid-cols-1 items-center justify-center gap-6">
                                         <Link
