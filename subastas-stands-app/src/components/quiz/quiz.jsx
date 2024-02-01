52c6e1d76f87d9d7f3bd4864ccdea1376e498dea
Diego Flores <dfmonroy07@gmai.com>
2024-02-01 15:03:29 -0600

    updated back connection

--- subastas-stands-app/src/components/quiz/quiz.jsx
+++ subastas-stands-app/src/components/quiz/quiz.jsx
@@ -21,7 +21,7 @@
         }
         useEffect(() => {
             if(showResult){
-                fetch('http://localhost:3000/answerQuiz', {
+                fetch('https://subasta-viajespalacio-24f6392aae86.herokuapp.com//answerQuiz', {
                     method: 'POST',
                     headers: {'Content-Type': 'application/json'},
                     body: JSON.stringify({
